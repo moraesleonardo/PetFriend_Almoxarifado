3307eb3b90644a52f457619e0c9b6ca09dbdfb8b
Leonardo Moraes <leonardobastosmoraes@hotmail.com>
2024-09-18 15:44:09 -0300

    Atualização do projeto

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 
-INSERT INTO pedido (cliente, data_pedido, status) VALUES ('Cliente B', '2024-09-18', 'EM_PREPARACAO');
+INSERT INTO pedido (cliente, data_pedido, status) VALUES ('Cliente A', '2024-09-18', 'EM_PREPARACAO');
 
